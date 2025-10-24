4a52ef8cbecc2b224917631ebaba8e25ce3a67e3
ALEKSANDROOOOO <103053621+ALEKSANDROOOOO@users.noreply.github.com>
2025-10-24 22:10:42 +0300

    Add files via upload

--- LR5/main.py
+++ LR5/main.py
@@ -1,4 +1,4 @@
-from typing import Callable 
+from typing import Callable, Dict, Any
 from collections import deque
 
 
@@ -22,11 +22,12 @@ def main() -> None:
     #l_b = lambda x : 2*(x+1) 
     #r_b = lambda x : 2*(x-1)
     
-    #output_bin_tree(gen_bin_tree()) # для красивого вывода - заменить в gen_bin_tree return (закомментирован)
-    #print(gen_bin_tree())
+    #output_bin_tree() # для красивого вывода
+    print(gen_bin_tree(6, 15, lambda x: 3*(x+1), lambda x: 3*(x-1)))
+    #output_bin_tree()
     
 
-def gen_bin_tree(height: int=6, root: int=15, l_b: int=lambda x: 2*(x+1), r_b: int=lambda x: 2*(x-1)) -> list[int]:
+def gen_bin_tree(height: int=6, root: int=15, l_b: Callable[[int], int]=lambda x: 2*(x+1), r_b: Callable[[int], int]=lambda x: 2*(x-1)) -> Dict[str, str | Any]:
     
     tree = [[] for i in range(height)]
     tree[0] = [root]
@@ -47,7 +48,7 @@ def gen_bin_tree(height: int=6, root: int=15, l_b: int=lambda x: 2*(x+1), r_b: i
     leaves = deque()
     
     for el in tree[-1]:
-        leaves.append({el: []})
+        leaves.append({str(el): []})
     
     for h in range(height-2, -1, -1):
         branch = deque()
@@ -56,14 +57,14 @@ def gen_bin_tree(height: int=6, root: int=15, l_b: int=lambda x: 2*(x+1), r_b: i
             left = leaves.popleft()
             right = leaves.popleft()
             
-            branch.append({el: [left, right]})
+            branch.append({str(el): [left, right]})
         
         leaves = branch
         
-    return leaves[0] # tree
+    return leaves[0] # leaves - deque(); leaves[0] - Dict
              
 
-def output_bin_tree(tree: list[list[int]], spaces: int = 128) -> None:
+def output_bin_tree(height: int=6, root: int=15, l_b: Callable[[int], int]=lambda x: 2*(x+1), r_b: Callable[[int], int]=lambda x: 2*(x-1), spaces: int = 128) -> None:
 
     """
     Красивый вывод дерева (списка tree), опираясь на математическую модель
@@ -71,13 +72,28 @@ def output_bin_tree(tree: list[list[int]], spaces: int = 128) -> None:
     Ничего не возвращает
     """
 
+    # Генерация для красивого вывода
+    tree = [[] for i in range(height)]
+    tree[0] = [root]
+
+    for h in range(1, height): 
+        n = 2**(h) 
+        # Кол-во элементов на ветке, четный эл - левая ветка, нечетный - правая
+        anc = 0 # номер корня-предка для двух веток
+
+        for i in range(0, n, 2):
+            
+            tree[h].append(l_b(tree[h-1][anc]))
+            tree[h].append(r_b(tree[h-1][anc]))
+            anc += 1
+
     # рассчет расстояния между числами
     interval_count = {'0': spaces//2, '1': spaces//4, '2':spaces//8, '3':spaces//16, '4':spaces//32, '5':spaces//64, '6':spaces//128}
 
     
     # вывод чисел
     print()
-    for i in range(len(tree)-1):
+    for i in range(len(tree)):
         print(str(i+1)+'й уровень: '+' '*10, end='')       
     
         interval = interval_count.get(str(i))
@@ -89,7 +105,7 @@ def output_bin_tree(tree: list[list[int]], spaces: int = 128) -> None:
             
 
         print(end='\n\n')
-        
+    
         
 
 
